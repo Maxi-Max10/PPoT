05dd9e5a994af150cfaea2a5a61ad657a8bda4d9
Maxi-Max10 <maximilianoalderete017@gmail.com>
2021-11-09 19:54:27 -0300

    PPoT

---
+++ PPoT/src/ppot/PPoT.java
@@ -0,0 +1,14 @@
+
+package ppot;
+
+import Frame.PiedraPapelTijera;
+
+
+public class PPoT {
+
+    public static void main(String[] args) {
+        PiedraPapelTijera.main(args);
+        
+    }
+
+}
